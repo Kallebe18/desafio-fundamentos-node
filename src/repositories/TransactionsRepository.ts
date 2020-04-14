180635045599df82f4025c00bcd9700d08855c16
Kallebe18 <kallebegomes18@gmail.com>
2020-04-13 23:55:29 -0300

    Ending challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    this.transactions.forEach(transaction => {
+      switch (transaction.type) {
+        default:
+          break;
+        case 'income':
+          balance.income += transaction.value;
+          break;
+        case 'outcome':
+          balance.outcome += transaction.value;
+          break;
+      }
+    });
+
+    balance.total = balance.income - balance.outcome;
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const balance = this.getBalance();
+    if (type === 'outcome' && value > balance.total)
+      throw Error('You dont have enough money for this transaction');
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
